4fba8b1a4fe5d7e347bf6a2e263564ff18444d33
Jeffrey Quizon <jeffrey.quizon@gmail.com>
2014-10-14 14:20:56 -0700

    Add newrelic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['AOL Chat App'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'c1f820d7def38b6cc24bfa95fea0c9b43519b25d',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
